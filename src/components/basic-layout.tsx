99cd4fab265115891f96eef14e35154fff738984
Du Jiajun <jiajun_du@outlook.com>
2025-06-28 22:10:54 +0800

    feat: bump year

--- src/components/basic-layout.tsx
+++ src/components/basic-layout.tsx
@@ -31,7 +31,7 @@ export const BasicLayout = () => {
             <Link to="/faq">常见问题</Link>
             <Link to="/report">反馈</Link>
           </Space>
-          <div>©2023 SJTU选课社区</div>
+          <div>©2025 SJTU选课社区</div>
         </Footer>
       </Layout>
     </CommonInfoContext.Provider>
